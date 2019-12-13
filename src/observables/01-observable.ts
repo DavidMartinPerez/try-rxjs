1511f7ae00ea491b8df29a287728379660f31926
David Martín Pérez <davidmartinperez1@gmail.com>
2019-12-13 23:07:47 +0100

    pretty

--- src/observables/01-observable.ts
+++ src/observables/01-observable.ts
@@ -1,27 +1,25 @@
 import { Observable, Observer } from 'rxjs';
 
 const observer: Observer<any> = {
-    next: value => console.log('[next]', value),
-    error: err => console.warn('[error]', err),
-    complete: () => console.info('[complete]')
+	next: (value) => console.log('[next]', value),
+	error: (err) => console.warn('[error]', err),
+	complete: () => console.info('[complete]')
 };
 
-const obs$ = new Observable<string>( subs => {
+const obs$ = new Observable<string>((subs) => {
+	subs.next('Hoola!');
+	subs.next('Mundo!');
 
-    subs.next( 'Hoola!' );
-    subs.next( 'Mundo!' );
+	//Error
+	// const a = undefined;
+	// a.name = 'Error name of undefined';
 
-    //Error
-    // const a = undefined;
-    // a.name = 'Error name of undefined';
+	subs.complete();
 
-    subs.complete();
-
-    subs.next('Este next no va a tener efecto porque el observable se completo');
-
+	subs.next('Este next no va a tener efecto porque el observable se completo');
 });
 
-obs$.subscribe( observer );
+obs$.subscribe(observer);
 
 // obs$.subscribe(
 //     value => console.log('next', value),
