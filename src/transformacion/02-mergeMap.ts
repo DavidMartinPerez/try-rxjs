eaa0edc8c34b528603a96651652707305aca5c11
David Martín Pérez <davidmartinperez1@gmail.com>
2019-12-17 17:47:25 +0100

    mergeMap

---
+++ src/transformacion/02-mergeMap.ts
@@ -0,0 +1,29 @@
+import { of, interval, fromEvent } from 'rxjs';
+import { mergeAll, mergeMap, map, take, takeUntil } from 'rxjs/operators';
+
+const letras$ = of('a','b','c');
+
+letras$.pipe(
+  mergeMap(
+    (letra) => interval(1000).pipe(
+      map( i => letra + i ),
+      take(3)
+    )
+  )
+)//.subscribe({
+//   next: (val) => console.log('[next] ->', val),
+//   complete: () => console.log('[complete]')
+// })
+
+const mousedown$ = fromEvent( document, 'mousedown' );
+const mouseup$ = fromEvent( document, 'mouseup' );
+const interval$ = interval(200);
+
+mousedown$.pipe(
+  mergeMap(
+    () => interval$.pipe(
+      takeUntil( mouseup$ )
+    )
+  )
+)
+.subscribe( x => console.log('[next] -> mousedown') )
