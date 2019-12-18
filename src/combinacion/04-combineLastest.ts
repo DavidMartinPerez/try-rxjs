879355ab96a94aec001a9ce0ad832cb040c14c3b
David Martín Pérez <davidmartinperez1@gmail.com>
2019-12-18 19:57:34 +0100

    example con inputs

--- src/combinacion/04-combineLastest.ts
+++ src/combinacion/04-combineLastest.ts
@@ -1,8 +1,16 @@
-import { combineLatest, of } from 'rxjs';
+import { combineLatest, of, fromEvent } from 'rxjs';
+import { pluck } from 'rxjs/operators';
 
-const letras$ = of('a','b','c','d');
-const numeros$ = of(1,2,3,4,5);
+const letras$ = of('a', 'b', 'c', 'd');
+const numeros$ = of(1, 2, 3, 4, 5);
 
-combineLatest(
-    letras$, numeros$
-).subscribe( console.log )
+combineLatest(letras$, numeros$).subscribe(console.log);
+
+const input1 = document.createElement('input');
+const input2 = document.createElement('input');
+document.querySelector('body').append(input1, input2);
+
+const getInputStream = (elem: HTMLElement) =>
+	fromEvent<KeyboardEvent>(elem, 'keyup').pipe(pluck<KeyboardEvent, string>('target', 'value'));
+
+combineLatest(getInputStream(input1), getInputStream(input2)).subscribe(console.log);
